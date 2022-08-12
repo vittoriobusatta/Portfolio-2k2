5e6404d825d1a7df85f0932f865663ba8a9e20ef
Vittorio <tonibusatta@gmail.com>
2022-08-12 14:38:59 +0400

    test with setTimeout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Loader from "./Components/Loader";
 import "./Assets/Styles//Fonts.css";
 import "./Assets/Styles/Reset.css";
 import styled, { createGlobalStyle } from "styled-components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ManropeMedium } from "./Utils/Common";
 import faviconDark from "./Assets/Icons/faviconDark.png";
 import faviconLight from "./Assets/Icons/faviconLight.png";
@@ -72,6 +72,7 @@ const ThemeButton = styled.button`
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const toggleTheme = () => {
     setDarkMode(!darkMode);
@@ -85,14 +86,24 @@ function App() {
   }
   link.href = darkMode === true ? faviconLight : faviconDark;
 
+  const onLoad = () => {
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    setTimeout(onLoad, 6000);
+  } , [])
+
+
+
   return (
     <>
       <GlobalStyle darkMode={darkMode} />
-      <Loader darkMode={darkMode} />
+      {loading === true ? <Loader /> : <Landing darkMode={darkMode} />}
       <ThemeButton colorTheme={darkMode} onClick={toggleTheme}>
         {darkMode ? "Light" : "Dark"}
       </ThemeButton>
-      <Landing darkMode={darkMode} />
+      
 
       {/* <path
             ref={R}
