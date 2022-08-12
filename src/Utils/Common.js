163d65d0f282ae7a179313652c96e2a4f991dd60
Vittorio <tonibusatta@gmail.com>
2022-08-12 23:01:44 +0400

    add new font

--- src/Utils/Common.js
+++ src/Utils/Common.js
@@ -1,3 +1,4 @@
 export const ManropeRegular = 'Manrope Regular';
 export const ManropeMedium = 'Manrope Medium';
 export const ManropeExtraBold = 'Manrope ExtraBold';
+export const RecoletaRegular = "Recoleta Regular";
