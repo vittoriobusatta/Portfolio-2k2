a0110abaad379e8e336d548cabee9d200e474e39
Vittorio <tonibusatta@gmail.com>
2022-12-04 16:44:48 +0400

    update img

--- src/Components/Projects/DecortaKaz.js
+++ src/Components/Projects/DecortaKaz.js
@@ -132,11 +132,11 @@ function DecortaKaz({ data }) {
             alt={projectData[0].images.view.first.alt}
           />
           <img
-            src={projectData[0].images.assets.sixth.image}
+            src={projectData[0].images.assets.fifth.image}
             alt={projectData[0].images.view.first.alt}
           />
-          <img
-            src={projectData[0].images.assets.fifth.image}
+            <img
+            src={projectData[0].images.assets.sixth.image}
             alt={projectData[0].images.view.first.alt}
           />
           <img
